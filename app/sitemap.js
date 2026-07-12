93e7e8297ace74a2b4ea4c801136662cb7faf5e9
veilautocare <veilautocare.1@gmail.com>
2026-07-12 00:04:34 -0700

    Add sitemap function to generate site map

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://veilautocare.com",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
