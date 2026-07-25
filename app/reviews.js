ba83bf13eac01b4cdc5a7fe4c6072a0bb0a10d96
veilautocare <veilautocare.1@gmail.com>
2026-07-25 00:38:39 -0700

    Add new Google reviews to reviews.js

--- app/reviews.js
+++ app/reviews.js
@@ -1,27 +1,32 @@
 export const googleReviews = [
 `agam sidhu
 ★★★★★
+July 20, 2026
 
 Couldn’t be happier with the service. I brought my Tesla Model 3 in and it came back looking better than the day I picked it up. You can tell they actually take their time and care about the little details instead of rushing through the job.`,
 
 `Gurjot Garcha
 ★★★★★
+July 20, 2026
 
 Naman takes pride in his work. Good service and fair price. Got 2 of our cars detailed with Veil and they came out looking straight out of a showroom!`,
 
 `Simar Dhiman
 ★★★★★
+July 14, 2026
 
 Really happy with the detail. The car came out super clean, and you can tell they took their time and paid attention to the details. Great service!`,
 
 `Gurman Gill
 ★★★★★
+July 12, 2026
 
 Fast, reliable, and efficient work at a reasonable price.`,
 
   
 `Arvin Gakhal
 ★★★★★
+July 25,2026
 
 Did an excellent job, brought my 80s classic back to showroom condition. Would recommend to everyone. Honest, genuine and reliable.`
 
