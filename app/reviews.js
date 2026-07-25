4d3f0f4f986966cc11da5a5186ca8cdbc15d03a5
veilautocare <veilautocare.1@gmail.com>
2026-07-25 00:26:10 -0700

    Add new review for Arvin Gakhal

--- app/reviews.js
+++ app/reviews.js
@@ -1,25 +1,28 @@
 export const googleReviews = [
 `agam sidhu
 ★★★★★
-4 days ago
 
 Couldn’t be happier with the service. I brought my Tesla Model 3 in and it came back looking better than the day I picked it up. You can tell they actually take their time and care about the little details instead of rushing through the job.`,
 
 `Gurjot Garcha
 ★★★★★
-4 days ago
 
 Naman takes pride in his work. Good service and fair price. Got 2 of our cars detailed with Veil and they came out looking straight out of a showroom!`,
 
 `Simar Dhiman
 ★★★★★
-1 week ago
 
 Really happy with the detail. The car came out super clean, and you can tell they took their time and paid attention to the details. Great service!`,
 
 `Gurman Gill
 ★★★★★
-1 week ago
 
 Fast, reliable, and efficient work at a reasonable price.`
+
+  
+Arvin Gakhal
+★★★★★
+
+Did an excellent job, brought my 80s classic back to showroom condition. Would recommend to everyone. Honest, genuine and reliable.
+
 ];
